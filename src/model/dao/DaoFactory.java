8ae5891f6e5ca0c5ce75f64a53691e259c78fd23
= <cleytongarattosousa@gmail.com>
2023-08-20 12:19:10 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,18 @@
+package model.dao;
+
+import model.dao.impl.SellerDaoJDBC;
+
+// Fábrica de DAO. Aqui tem operações estáticas
+// para instanciar os DAOs
+
+public class DaoFactory {
+
+	// Esse é um macete pra não excpor a implementação
+	// Apenas a interface.
+	// É também uma forma de fazer uma injeção de dependência,
+	// sem explicitar a implementação
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC();
+	}
+	
+}
